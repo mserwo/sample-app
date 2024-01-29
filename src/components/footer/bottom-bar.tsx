446a5b0189ae62efe76838585604ee36f113dbd1
Marcin Serwotka <marcin.serwotka@op.pl>
2024-01-29 16:30:36 +0100

    feat: bottom bar commit

--- src/components/footer/bottom-bar.tsx
+++ src/components/footer/bottom-bar.tsx
@@ -3,12 +3,17 @@ import styles from "./footer.module.scss";
 const BottomBar = () => {
   return (
     <div className={styles.bottomBar}>
-      <div className={styles.bottomBar_leftText}>
-        {"Copyright © 2021 UI8 LLC. All rights reserved"}
-      </div>
-      <div>
-        <div>{"We use cookies for better service."}</div>
-        <div>{"Decline"}</div>
+      <div className={styles.bottomBar_divider}></div>
+      <div className={styles.bottomBar_content}>
+        <div className={styles.bottomBar_leftText}>
+          {"Copyright © 2021 UI8 LLC. All rights reserved"}
+        </div>
+        <div className={styles.bottomBar_rightFrame}>
+          <div className={styles.bottomBar_rightText}>
+            {"We use cokkies for better service."}
+          </div>
+          <div className={styles.bottomBar_accept}>{"Accept"}</div>
+        </div>
       </div>
     </div>
   );
