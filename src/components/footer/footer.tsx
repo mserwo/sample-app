34b33a399522af314e82a4b0c7f0a9ac3c5a9b02
Marcin Serwotka <marcin.serwotka@op.pl>
2024-02-01 21:52:11 +0100

    feat: added navigation menu component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,24 @@
 import styles from "./footer.module.scss";
 import { BottomBar } from "./bottom-bar/bottom-bar";
 import LogoSection from "./logo-section/logo-section";
+import { NavigationMenu } from "./navigation-menu/navigation-menu";
+
+const menu1items = [
+  { title: "Discover", url: "/discover" },
+  { title: "Connect wallet", url: "/connect-wallet" },
+  { title: "Create item", url: "/create-item" },
+];
 
 export const Footer = () => {
   return (
     <footer className={styles.wrapper}>
       <div className={styles.container}>
-        <LogoSection />
+        <div className={styles.content}>
+          <LogoSection />
+          <NavigationMenu title={"Stacks"} menuElements={menu1items} />
+          <div>test</div>
+        </div>
+
         <BottomBar />
       </div>
     </footer>
