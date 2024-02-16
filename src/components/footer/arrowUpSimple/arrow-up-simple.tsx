35ee3d4c16a290dcae1ebf770d7ee926a0b3bb06
Gabriel Litwiński <61962677+GabrLit@users.noreply.github.com>
2024-02-16 13:13:49 +0100

    Revert "Feat/navigation menu, breakpoints (#5)"
    This reverts commit 210f1f732bde1cad059dc7e573e73e0271cda99e.

--- src/components/footer/arrowUpSimple/arrow-up-simple.tsx
+++
@@ -1,6 +0,0 @@
-import styles from "./arrow-up-simple.module.scss";
-import arrowUpSimple from "../../../assets/images/arrowUpSimple.png";
-
-export const ArrowUpSimple = () => {
-  return <img className={styles.img} src={arrowUpSimple}></img>;
-};
--- src/components/footer/navigation-menu/navigation-menu.tsx
+++ src/components/footer/navigation-menu/navigation-menu.tsx
@@ -1,7 +1,4 @@
-import { ArrowDownSimple } from "../arrowDownSimple/arrow-down-simple";
-import { ArrowUpSimple } from "../arrowUpSimple/arrow-up-simple";
 import styles from "./navigation-menu.module.scss";
-import { useState } from "react";
 
 interface NavigationMenuProps {
   title: string;
@@ -12,25 +9,9 @@ export const NavigationMenu = ({
   title,
   menuElements,
 }: NavigationMenuProps) => {
-  const [arrow, setArrow] = useState<string>("down");
-
-  const ArrowChange = (id) => {
-    console.log(id);
-    setArrow("up");
-    console.log(arrow);
-  };
-
   return (
     <div className={styles.container}>
-      <div className={styles.container_top}>
-        <div className={styles.container_title}>{title}</div>
-        <div className={styles.container_arrow}>
-          <ArrowDownSimple task={ArrowChange} id={menuElements} />
-          <ArrowUpSimple />
-          {arrow}
-        </div>
-      </div>
-
+      <div className={styles.container_title}>{title}</div>
       <ul className={styles.container_menu}>
         {menuElements.map((item) => (
           <li>
