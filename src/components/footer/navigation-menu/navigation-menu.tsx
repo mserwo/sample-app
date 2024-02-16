210f1f732bde1cad059dc7e573e73e0271cda99e
Marcin Serwotka <148576179+mserwo@users.noreply.github.com>
2024-02-16 10:15:33 +0100

    Feat/navigation menu, breakpoints (#5)
    * feat: added navigation menu component
    
    * feat: navigation menu component, breakpoints

--- src/components/footer/navigation-menu/navigation-menu.tsx
+++ src/components/footer/navigation-menu/navigation-menu.tsx
@@ -1,4 +1,7 @@
+import { ArrowDownSimple } from "../arrowDownSimple/arrow-down-simple";
+import { ArrowUpSimple } from "../arrowUpSimple/arrow-up-simple";
 import styles from "./navigation-menu.module.scss";
+import { useState } from "react";
 
 interface NavigationMenuProps {
   title: string;
@@ -9,9 +12,25 @@ export const NavigationMenu = ({
   title,
   menuElements,
 }: NavigationMenuProps) => {
+  const [arrow, setArrow] = useState<string>("down");
+
+  const ArrowChange = (id) => {
+    console.log(id);
+    setArrow("up");
+    console.log(arrow);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.container_title}>{title}</div>
+      <div className={styles.container_top}>
+        <div className={styles.container_title}>{title}</div>
+        <div className={styles.container_arrow}>
+          <ArrowDownSimple task={ArrowChange} id={menuElements} />
+          <ArrowUpSimple />
+          {arrow}
+        </div>
+      </div>
+
       <ul className={styles.container_menu}>
         {menuElements.map((item) => (
           <li>
