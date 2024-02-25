f58b5bc8b1a2cd2f9cc20de0efac5e485a84f6d3
Marcin Serwotka <marcin.serwotka@op.pl>
2024-02-25 20:00:20 +0100

    feat: navigation menu component - arrow rotate

--- src/components/footer/navigation-menu/navigation-menu.tsx
+++ src/components/footer/navigation-menu/navigation-menu.tsx
@@ -1,8 +1,9 @@
 import styles from "./navigation-menu.module.scss";
 import { useState } from "react";
 import { ReactComponent as ArrowDownSimpleIcon } from "../../../assets/images/arrowDownSimple.svg";
-import { ReactComponent as ArrowUpSimpleIcon } from "../../../assets/images/arrowUpSimple.svg";
+
 import classNames from "classnames";
+import { useScreenWidth } from "../../../hooks";
 
 interface NavigationMenuProps {
   title: string;
@@ -16,7 +17,9 @@ export const NavigationMenu = ({
   const [isArrowDownClick, setIsArrowDownClick] = useState(false);
   const [isRollUp, setIsRollUp] = useState(false);
 
-  const ArrowChange = () => {
+  const isMobile = useScreenWidth() < 768;
+
+  const handleArrowChange = () => {
     setIsArrowDownClick(!isArrowDownClick);
     {
       !isArrowDownClick
@@ -30,18 +33,25 @@ export const NavigationMenu = ({
       <div className={styles.container_top}>
         <div className={styles.container_title}>{title}</div>
         <div className={styles.container_arrow}>
-          <div className={styles.img} onClick={ArrowChange}>
-            {isArrowDownClick ? <ArrowUpSimpleIcon /> : <ArrowDownSimpleIcon />}
+          <div
+            className={styles.img}
+            style={{
+              transform: `rotate(${isRollUp ? 180 : 0}deg`,
+              transition: "all 0.25s",
+            }}
+            onClick={handleArrowChange}
+          >
+            <ArrowDownSimpleIcon />
           </div>
         </div>
       </div>
       <ul
         className={classNames(styles.container_menu, {
-          [styles.container_menu_hide]: isRollUp,
+          [styles.container_menu_hide]: isRollUp && isMobile,
         })}
       >
         {menuElements.map((item) => (
-          <li>
+          <li key={item.title + item.url}>
             <a href={item.url}>{item.title}</a>
           </li>
         ))}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Layout } from "../../components/layout";
+import { useScreenWidth } from "../../hooks";
 
 export const Home = () => {
+  useScreenWidth();
   return (
     <Layout>
       <div>home</div>
