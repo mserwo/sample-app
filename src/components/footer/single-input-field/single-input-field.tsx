faa88accac594094b86f3b6dc9dc67159a79b2c1
Marcin Serwotka <marcin.serwotka@op.pl>
2024-02-29 21:32:12 +0100

    feat(navigation-menu):fix2

--- src/components/footer/single-input-field/single-input-field.tsx
+++ src/components/footer/single-input-field/single-input-field.tsx
@@ -1,53 +1,15 @@
 import styles from "./single-input-field.module.scss";
 import { ReactComponent as ArrowRight2 } from "../../../assets/images/arrowRight2.svg";
-import { useState } from "react";
 
 export const SingleInputField = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [error, setError] = useState(false);
-  const [isArrowClick, setIsArrowClick] = useState(false);
-  const [emailArr, setEmailArr] = useState([]);
-
-  const handleEmailButton = () => {
-    inputValue.length === 0
-      ? setError(true)
-      : (setError(false), setInputValue(""), addToEmailArr());
-    setIsArrowClick(true);
-  };
-
-  const addToEmailArr = () => {
-    if (!error && inputValue && isArrowClick) {
-      setEmailArr([...emailArr, inputValue]), console.log(emailArr);
-    }
-  };
-
   return (
     <div>
       <div className={styles.container}>
         <input
           className={styles.container_input}
           placeholder="Enter your email"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
         ></input>
-        <ArrowRight2
-          className={styles.container_arrow}
-          onClick={handleEmailButton}
-        />
-      </div>
-      <div className={styles.inputAnswer}>
-        {isArrowClick ? (
-          error ? (
-            <div className={styles.inputAnswer_error}>
-              {" "}
-              The field cannot be empty{" "}
-            </div>
-          ) : (
-            <div className={styles.inputAnswer_correct}>
-              Thank you! Your email address has been successfully saved.
-            </div>
-          )
-        ) : null}
+        <ArrowRight2 className={styles.container_arrow} />
       </div>
     </div>
   );
