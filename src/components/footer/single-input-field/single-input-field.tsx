b4f16288a33f71a5ea5df060cc1a34dd34249f52
Marcin Serwotka <marcin.serwotka@op.pl>
2024-02-29 20:00:19 +0100

    feat(join-newsletter): first commit

--- src/components/footer/single-input-field/single-input-field.tsx
+++ src/components/footer/single-input-field/single-input-field.tsx
@@ -1,41 +1,34 @@
 import styles from "./single-input-field.module.scss";
 import { ReactComponent as ArrowRight2 } from "../../../assets/images/arrowRight2.svg";
-// import { useState } from "react";
+import { useState } from "react";
 
 export const SingleInputField = () => {
-  // const [inputValue, setInputValue] = useState("");
-  // const [error, setError] = useState(false);
-  // const [isArrowClick, setIsArrowClick] = useState(false);
-  // const [emailArr, setEmailArr] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState(false);
+  const [isArrowClick, setIsArrowClick] = useState(false);
 
-  // const handleEmailButton = () => {
-  //   inputValue.length === 0
-  //     ? setError(true)
-  //     : (setError(false), setInputValue(""), addToEmailArr());
-  //   setIsArrowClick(true);
-  // };
-
-  // const addToEmailArr = () => {
-  //   if (!error && inputValue && isArrowClick) {
-  //     setEmailArr([...emailArr, inputValue]), console.log(emailArr);
-  //   }
-  // };
+  const handleEmailButton = () => {
+    inputValue.length === 0
+      ? setError(true)
+      : (setError(false), setInputValue(""));
+    setIsArrowClick(true);
+  };
 
   return (
     <div>
-      <div className={styles.container}>
+      <div className={styles.container} onSubmit={() => {}}>
         <input
           className={styles.container_input}
           placeholder="Enter your email"
-          // value={inputValue}
-          // onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         ></input>
         <ArrowRight2
           className={styles.container_arrow}
-          // onClick={handleEmailButton}
+          onClick={handleEmailButton}
         />
       </div>
-      {/* <div className={styles.inputAnswer}>
+      <div className={styles.inputAnswer}>
         {isArrowClick ? (
           error ? (
             <div className={styles.inputAnswer_error}>
@@ -48,7 +41,7 @@ export const SingleInputField = () => {
             </div>
           )
         ) : null}
-      </div> */}
+      </div>
     </div>
   );
 };
