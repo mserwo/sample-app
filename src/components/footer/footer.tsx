b552525f8f78081723a467685fb4b1ec59503217
Marcin Serwotka <148576179+mserwo@users.noreply.github.com>
2024-03-05 21:36:31 +0100

    feat(repair/navigation-bar): added navigation bar (#15)

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,19 @@
 import styles from "./footer.module.scss";
 import { BottomBar } from "./bottom-bar/bottom-bar";
 import LogoSection from "./logo-section/logo-section";
+import { NavigationMenu } from "./navigation-menu/navigation-menu";
+
+const menu1items = [
+  { title: "Discover", url: "/discover" },
+  { title: "Connect wallet", url: "/connect-wallet" },
+  { title: "Create item", url: "/create-item" },
+];
+
+const menu2items = [
+  { title: "Download", url: "/download" },
+  { title: "Demos", url: "/demos" },
+  { title: "Support", url: "/support" },
+];
 
 export const Footer = () => {
   return (
@@ -8,6 +21,8 @@ export const Footer = () => {
       <div className={styles.container}>
         <div className={styles.content}>
           <LogoSection />
+          <NavigationMenu title={"Stacks"} menuElements={menu1items} />
+          <NavigationMenu title={"Info"} menuElements={menu2items} />
         </div>
         <BottomBar />
       </div>
