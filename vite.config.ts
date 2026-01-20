c7323108d2ec744fc5ac40363c923187c7f3e2f0
Marcin Serwotka <marcin.serwotka@op.pl>
2026-01-20 19:42:28 +0100

    Fix GH Pages build with BrowserRouter basename

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
+  base: isGhPages ? "/sample-app/" : "/",
   resolve: {
     alias: {
       "~": "/src/styles",
